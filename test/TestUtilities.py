4392dbbf47b864c07b18344a74553b61155e0a95
Spock <Golbaghali@home>
2017-12-07 19:47:38 -0500

    improved logs schema

--- test/TestUtilities.py
+++ test/TestUtilities.py
@@ -27,33 +27,46 @@ class TestUtilities (unittest.TestCase):
 	def test_dataloader (self):
 		datafile = 'data/wslog.dat.template'
 		data = utils.dataloader (datafile, ApacheLogSchemaRaw, ' ')
+		actual = data[0]
+		expected = {'ip': '198.0.200.105', 
+				'user_ident': '-',
+				'user_http': '-', 
+				'ts': '[14/Jan/2014:09:36:50', 
+				'ms': '-0800]', 
+				'request': 'GET /svds.com/rockandroll HTTP/1.1', 
+				'response_code': '301', 
+				'bytes_out': '241', 
+				'referrer': '-',
+				'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
+		self.assertEqual(actual, expected)
+
 		expected_len = 20
 		self.assertEqual (len(data), expected_len)
 		
 	def test_cleanselog (self):
-		log = {'f0_ip': '198.0.200.105', 
-				'f1_user_ident': '-', 
-				'f2_user_http': '-', 
-				'f3_ts': '[14/Jan/2014:09:36:50', 
-				'f4_ms': '-0800]', 
-				'f5_request': 'GET /svds.com/rockandroll HTTP/1.1', 
-				'f6_response_code': '301', 
-				'f7_bytes_out': '241', 
-				'f8_referrer': '-', 
-				'f9_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
+		log = {'ip': '198.0.200.105', 
+				'user_ident': '-', 
+				'user_http': '-', 
+				'ts': '[14/Jan/2014:09:36:50', 
+				'ms': '-0800]', 
+				'request': 'GET /svds.com/rockandroll HTTP/1.1', 
+				'response_code': '301', 
+				'bytes_out': '241', 
+				'referrer': '-', 
+				'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
 		actual = ApacheLog.cleanselog(log)
-		expected = {'f0_ip': '198.0.200.105',
-				'f10_request_resource': '/svds.com/rockandroll',
-				'f11_request_protocol': 'HTTP/1.1',
-				'f1_user_ident': '-',
-				'f2_user_http': '-',
-				'f3_ts': '14/Jan/2014:09:36:50',
-				'f4_ms': '-0800',
-				'f5_request_method': 'GET',
-				'f6_response_code': '301',
-				'f7_bytes_out': '241',
-				'f8_referrer': '-',
-				'f9_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
+		expected = {'ip': '198.0.200.105',
+				'request_resource': '/svds.com/rockandroll',
+				'request_protocol': 'HTTP/1.1',
+				'user_ident': '-',
+				'user_http': '-',
+				'ts': '14/Jan/2014:09:36:50',
+				'ms': '-0800',
+				'request_method': 'GET',
+				'response_code': '301',
+				'bytes_out': '241',
+				'referrer': '-',
+				'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
 		self.assertEqual (actual, expected)
 
 	def test_cleaselog_null (self):
